1e3cb65e92f4ce63d602cce3b7d40db1c9d61ad1
andrey <captain.destroyer@yandex.ru>
2021-07-01 21:31:00 +0700

    added html about page

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200);
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
 
 const PORT = process.env.PORT || 3000;
