8b97b06e0eb77d31bee3fb81dca8eac1bf857550
andrey <captain.destroyer@yandex.ru>
2021-07-14 16:20:40 +0700

    card for vapes

--- routes/vapes.js
+++ routes/vapes.js
@@ -1,11 +1,15 @@
 const { Router } = require('express');
+const Vape = require('../model/vape');
 
 const router = Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const vapes = await Vape.getAll();
+
   res.render('vapes', {
     title: 'Вейпы',
     isVapesPage: true,
+    vapes,
   });
 });
 
